dce7ed928ad1f52c6bcf054dc93466c0744ea4fc
Muhammad Fahmi Attaqwa <muhammad.attaqwa@students.amikom.ac.id>
2020-05-07 04:48:31 +0700

    class canon

---
+++ Canon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polymorphism
+{
+   public class Canon : printerWindows
+ {
+    public override void tmpl()
+    {
+        Console.Write("Canon display dimension : ");
+        string nomorPrinter = Console.ReadLine();
+    }
+
+    public override void print()
+    {
+        Console.WriteLine("Canon printer printing ....");
+    }
+ }
+}
