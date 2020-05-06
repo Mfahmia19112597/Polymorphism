afeb8d7d85b87c9a5290bf5beffee329903b9fb1
Muhammad Fahmi Attaqwa <muhammad.attaqwa@students.amikom.ac.id>
2020-05-07 04:53:26 +0700

    class induk

---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class printerWindows
+ {
+    public string name { get; set; }
+    public virtual void tmpl()
+    {
+        Console.WriteLine("Input Error");
+    }
+    public virtual void print()
+    {
+        //Console.WriteLine("Input Error");
+    }
+  }
+}
