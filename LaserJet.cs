51e0ec47aa99109fd43886c7b47b9c40f04e4f52
Muhammad Fahmi Attaqwa <muhammad.attaqwa@students.amikom.ac.id>
2020-05-07 04:51:46 +0700

    class LaserJet

---
+++ LaserJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polymorphism
+{
+  
+   public class LaserJet : printerWindows
+ {
+    public override void tmpl()
+    {
+        Console.Write("Laserjet display dimension : ");
+        string nomorPrinter = Console.ReadLine();
+    }
+    public override void print()
+    {
+        Console.WriteLine("LaserJet printer printing ....");
+    }
+  }
+}
