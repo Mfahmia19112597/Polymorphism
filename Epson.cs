203472068cfe985b42d54e9240a6e8af1c0e7d85
Muhammad Fahmi Attaqwa <muhammad.attaqwa@students.amikom.ac.id>
2020-05-07 04:50:48 +0700

    class epson

---
+++ Epson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class Epson : printerWindows
+ {
+    public override void tmpl()
+    {
+        Console.Write("Epson display dimension : ");
+        string nomorPrinter = Console.ReadLine();
+    }
+    public override void print()
+    {
+        Console.WriteLine("Epson printer printing ....");
+    }
+  }
+}
